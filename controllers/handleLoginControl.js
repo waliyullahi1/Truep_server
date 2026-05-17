eced50a105a6afa53d085de3149080cb3799967c
Quadri4423 <isholaolateju2015@gmail.com>
2026-05-17 08:07:01 +0100

    update

--- controllers/handleLoginControl.js
+++ controllers/handleLoginControl.js
@@ -223,7 +223,7 @@ export const logout = async (req, res) => {
     res.clearCookie('jwt', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
     })
 
     return res.status(200).json({
